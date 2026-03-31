f8970336d72aef82d8a6b220658fb775f23166d9
Nini <ny.randrianandrasana@eleve.egd.mg>
2026-03-31 17:44:09 +0200

    Tp2 milieu

--- src/Personnages/Romain.java
+++ src/Personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() == true;
 	}
 
 	public String getNom() {
@@ -20,15 +21,30 @@ public class Romain {
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
-	
+
 	public void recevoirCoup(int forceCoup) {
 		force = force - forceCoup;
-		if (force<1) {parler("J'abandonne");
-			
-		} else {parler("Aie!");
+		if (force < 1) {
+			parler("J'abandonne");
 
+		} else {
+			parler("Aie!");
+
+		}
+		assert isInvariantVerified() == true;
+	}
+
+	private boolean isInvariantVerified() {
+		if (force >= 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
+	public static void main(String[] args) {
+		Romain minus;
+		minus = new Romain("Minus", 6);
+	}
 
 }
