c2a595312914df69d9ba3479da0c2e2d5dea2152
Nini <ny.randrianandrasana@eleve.egd.mg>
2026-03-31 13:32:24 +0200

    Tp2 début

---
+++ src/village_gaulois/Village.java
@@ -0,0 +1,24 @@
+package village_gaulois;
+
+import Personnages.Gaulois;
+
+public class Village {
+	private String nom;
+	private Gaulois chef;
+	private Gaulois villageois;
+	private int nbVillageois = 0 ;
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public Gaulois getChef() {
+		return chef;
+	}
+	
+	public Gaulois ajouterVillageois(Gaulois gaulois) {
+	}
+	
+	
+
+}
