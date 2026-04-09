258eaa842bfd46e93740a6676ab409dd35c79efd
Nini <ny.randrianandrasana@eleve.egd.mg>
2026-04-09 13:16:51 +0200

    Tp2 fin

--- src/Personnages/Romain.java
+++ src/Personnages/Romain.java
@@ -1,13 +1,15 @@
 package Personnages;
-
+import objects.*;
 public class Romain {
 	private String nom;
 	private int force;
+	private int nbEquipement = 0;
+	private Equipement[] tab = new Equipement[2];
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		assert isInvariantVerified() == true;
+		assert isInvariantVerified(force) == true:"la force est positif";
 	}
 
 	public String getNom() {
@@ -23,7 +25,10 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
-		force = force - forceCoup;
+		assert isInvariantVerified(forceCoup): "la force du coup reçu est positif";
+		int forceAvant;
+		forceAvant = force;
+		force = forceAvant - forceCoup;
 		if (force < 1) {
 			parler("J'abandonne");
 
@@ -31,20 +36,47 @@ public class Romain {
 			parler("Aie!");
 
 		}
-		assert isInvariantVerified() == true;
+		assert forceAvant != force:"force a diminué";
 	}
 
-	private boolean isInvariantVerified() {
+	private boolean isInvariantVerified(int force) {
 		if (force >= 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
+	public void sEquiper(Equipement equipement) {
+		if (nbEquipement >= 2) {
+			System.out.println("Le  soldat " + getNom()+" est déjà bien protégé !");
+			
+		}else if(nbEquipement == 1) {
+			if (tab[0] == equipement) {
+				System.out.println("Le  soldat " + getNom()+" possède déjà un " + equipement.toString());
+			} else {
+				tab[1] = equipement;
+				System.out.println("Le  soldat " + getNom()+" s'equipe avec un " + equipement.toString());
+				nbEquipement ++;
+			}
+		} else {
+			tab[0] = equipement;
+			System.out.println("Le  soldat " + getNom()+" s'equipe avec un " + equipement.toString());
+			nbEquipement ++;
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		Romain minus;
 		minus = new Romain("Minus", 6);
+		for (Equipement equipement : Equipement.values()) {
+			System.out.println("- " + equipement);
+		}
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 
 }
---
+++ src/objects/Equipement.java
@@ -0,0 +1,15 @@
+package objects;
+
+public enum Equipement {
+	CASQUE("casque"), BOUCLIER("bouclier");
+
+	private String nom;
+
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	public String toString() {
+		return nom;
+	}
+}
